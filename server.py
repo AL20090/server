6feb9930afa437f70e46982f675aba08af0c9f2e
AL20090 <al20090@shibaura-it.ac.jp>
2023-03-13 16:05:37 +0900

    313v1

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask, request,render_template
+from flask import Flask,request,render_template
+
 app=Flask(__name__)
 file_path="./sensor_data.csv"
 port_num=20090
@@ -9,29 +10,30 @@ def get_html():
 
 @app.route('/lux',methods=['POST'])
 def update_lux():
-    time=request.form["time"]
-    lux=request.form["lux"]
+    time=request.form['time']
+    lux=request.form['lux']
     try:
         f=open(file_path,'w')
-        f.write(time+","+lux)
+        f.write(time+","+lux) 
         return "succeeded to write"
     except Exception as e:
-        print(e)
-        return "failed to write"
+           print(e)
+           return "failed to write"
     finally:
-        f.close()
+         f.close()
 
 @app.route('/lux',methods=['GET'])
 def get_lux():
-    try:
-        f=open(file_path,'r')
-        for row in f:
-            lux=row
-    except Exception as e:
-        print(e)
-    finally:
-        f.close()
-        return lux
+     try:
+          f=open(file_path,'r')
+          for row in f:
+               lux=row
 
-if __name__=='__main__':
-    app.run(debug=True,host='0.0.0.0',port=port_num)
+     except Exception as e:
+          print(e)
+     finally:
+          f.close()
+          return lux
+     
+if __name__== '__main__':
+     app.run(debug=True,host='0.0.0.0',port=port_num)
